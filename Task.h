4b800df45dc919c25e9bf3c01223ae96e7f91637
aisha <saisha248@gmail.com>
2024-11-17 01:23:50 -0800

    Create Task.h

---
+++ Task.h
@@ -0,0 +1,24 @@
+/*
+class Task{
+    Task(); //constructor
+    Task(string name, int priorityLevel, int deadline);
+    // getter and setter methods for when user wants to edit task
+    
+    private:
+    int name;
+    int priorityLevel;
+    int deadline;
+}
+
+class TaskList{
+    TaskList();
+    new task()
+    delete task()
+    sort tasks()
+    edit task() ?
+    printlist()
+
+    private:
+    vector Tasks
+}
+*/
